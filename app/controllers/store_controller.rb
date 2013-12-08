950b9578445312a222c0ff6fadb4c34c8e908f80
John Landes <jmlandes@containerstore.com>
2013-12-08 09:49:48 -0600

    Add page visit counter to store controller and index page, add page visit display test

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = session_count
+
+  end
+
+  def session_count
+    (session[:counter] = 0) if session[:counter].nil?
+    session[:counter] += 1
   end
 end
