64dbb014d8eba9c07a5da9ae531382f7be7d6d79
John Landes <jmlandes@containerstore.com>
2014-01-08 15:25:42 -0600

    Move test to Cart instead of Cart Controller

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -48,24 +48,4 @@ class CartsControllerTest < ActionController::TestCase
     assert_redirected_to store_path
   end
 
-  test "add unique products to cart" do
-    cart = Cart.create
-    bcd = products(:bcd)
-    reg  = products(:regulator)
-    cart.add_product(bcd.id).save!
-    cart.add_product(reg.id).save!
-    assert_equal 2, cart.line_items.size
-    assert_equal bcd.price + reg.price, cart.total_price
-  end
-
-  test "add duplicate product to cart" do
-    cart = Cart.create
-    bcd = products(:bcd)
-    cart.add_product(bcd.id).save!
-    cart.add_product(bcd.id).save!
-    assert_equal 2* bcd.price, cart.total_price
-    assert_equal 1, cart.line_items.size
-    assert_equal 2, cart.line_items[0].quantity
-  end
-
 end
